b484bbbad9acfb010e86640baf4a686fa8a6db35
aleks47 <alecsciocan@gmail.com>
2026-09-10 14:36:33 +0300

    Added required to the guess input

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -3,21 +3,17 @@ import React from "react";
 function GuessInput({ onGuessSubmit }) {
   const [guess, setGuess] = React.useState("");
 
-  function handleInputSubmit() {
+  function handleInputSubmit(event) {
+    event.preventDefault();
     onGuessSubmit(guess);
+    setGuess("");
   }
 
   return (
-    <form
-      className="guess-input-wrapper"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleInputSubmit();
-        setGuess("");
-      }}
-    >
+    <form className="guess-input-wrapper" onSubmit={handleInputSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
         id="guess-input"
         type="text"
         pattern="[A-Z]{5}"
