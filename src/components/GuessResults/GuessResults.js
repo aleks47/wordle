097fe869082bcfafe6da143e9992d26756465001
aleks47 <alecsciocan@gmail.com>
2026-09-10 14:51:12 +0300

    Replace hardcoded value

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,11 @@
 import Guess from "../Guess";
 import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ guessResults }) {
   return (
     <div className="guess-results">
-      {range(6).map((i) => (
+      {range(NUM_OF_GUESSES_ALLOWED).map((i) => (
         <Guess key={i} guessResult={guessResults[i]} />
       ))}
     </div>
